aa903961106f7266e19a7df5dcba1c5bcaa12c86
Phillip King <phillip.king@silverstripe.com>
2021-05-13 17:20:29 +1200

    Initial structure

---
+++ src/Controllers/RobotsController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Heyimphil\Robotson\Controllers;
+
+
+use SilverStripe\Control\Controller;
+
+class RobotsController extends Controller
+{
+    public function index()
+    {
+        return '';
+    }
+}
